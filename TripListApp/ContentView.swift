a66f9c224ffce585947666b646c722b354f0c126
Chamika <chamikachathushka8@gmail.com>
2023-11-10 18:03:59 +0530

    Initial Commit

---
+++ TripListApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TripListApp
+//
+//  Created by Chamika Sakalasuriya on 2023-11-10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TripListApp/TripListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripListAppApp.swift
+//  TripListApp
+//
+//  Created by Chamika Sakalasuriya on 2023-11-10.
+//
+
+import SwiftUI
+
+@main
+struct TripListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
